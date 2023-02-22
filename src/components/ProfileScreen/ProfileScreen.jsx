24b61cca588b472b6254e5898e96e992f8913e7e
jimish1806 <jimish957@gmail.com>
2023-02-22 22:42:53 +0530

    creating profile screen

--- src/components/ProfileScreen/ProfileScreen.jsx
+++ src/components/ProfileScreen/ProfileScreen.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './ProfileScreen.css'
+import profileImage from '../../assets/hacker.png'
+import netflixLogo from '../../assets/netflix-logo.png'
 
 const ProfileScreen = () => {
   return (
     <div className="profileScreen">
 
-        
+        <nav>
+            <img src={netflixLogo} alt="netflix Logo" />
+            <img src={profileImage} alt="Profile Pic" />
+        </nav>
+
+        <h1>Edit Profile</h1>
+        <hr />
+        <div className="profileContainer">
+            <img src={profileImage} alt="Profile logo" />
+        </div>
     </div>
   )
 }
